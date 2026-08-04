201846931e37e8de078dc63eead864207ece886e
utyara3 <prorok2018pro@gmail.com>
2026-08-04 23:44:39 +0300

    add logging and health endpoint

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from contextlib import asynccontextmanager
 
 from src.api.v1.contest_lists import router as university_router
+from src.core.logger_config import setup_logger
+from src.core.database import get_db
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app):
+    setup_logger("main")
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(university_router)
 
@@ -10,3 +24,10 @@ app.include_router(university_router)
 @app.get("/")
 async def root():
     return {"status": 200}
+
+
+@app.get("/health")
+async def health(db: AsyncSession = Depends(get_db)):
+    res = await db.execute(text("SELECT 1"))
+
+    return {"status": "healthy", "database": "connected", "result": res.scalar()}
