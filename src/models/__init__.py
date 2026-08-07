bdc2430a636d423dc77c7866741d96f8869e475d
utyara3 <prorok2018pro@gmail.com>
2026-08-07 19:01:39 +0300

    models summary

---
+++ src/models/__init__.py
@@ -0,0 +1,11 @@
+from .applicant import Applicant
+from .application import Application
+from .direction import Direction
+from .university import University
+
+__all__ = [
+    "Applicant",
+    "Application",
+    "Direction",
+    "University",
+]
