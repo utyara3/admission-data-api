f208785016ea5455f425f60474e37662bb01e4e4
utyara3 <prorok2018pro@gmail.com>
2026-09-04 11:26:41 +0300

    refactor(nix): add flakes for my nixvim ide

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Development environment for Admission Data API";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      pythonEnv = pkgs.python314.withPackages (ps: with ps; [
+        fastapi
+        uvicorn
+        httpx
+        beautifulsoup4
+        playwright
+        pydantic
+        pydantic-settings
+        sqlalchemy
+        asyncpg
+        alembic
+      ]);
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ pythonEnv ];
+      };
+    };
+}
+
